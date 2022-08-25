c2ea63b0c228f50bd031052bc5304b7e7db1d6d0
purelocation <109636666+purelocation@users.noreply.github.com>
2022-08-25 22:12:56 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuBar = document.querySelector(".menu-bar");
+const menunaV = document.querySelector(".menu-navigation");
+
+
+menuBar.addEventListener('click' , () => { }){do {
+	
+	
+} whilearray.forEach(element => {
+	 (conditi
+});on);
+}
